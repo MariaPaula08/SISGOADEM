f2fff9bb54ae2759e8d8eebd6924aa80d967869c
Sergio14 <3105984187.sanf@gmail.com>
2023-08-17 09:21:42 -0500

    Modificacion 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = Router()
 
 router.get('/',(req, res) => res.render('index', {title: 'SISGOADEM'}))
 router.get('/somos',(req, res) => res.render('somos', {title: 'Quienes Somos'}))
-router.get('/proyecto',(req, res) => res.render('proyecto', {title: 'Proyecto'}))
+router.get('/proyectos',(req, res) => res.render('proyectos', {title: 'Proyectos'}))
 router.get('/contacto',(req, res) => res.render('contacto', {title: 'Contacto'}))
 
 
